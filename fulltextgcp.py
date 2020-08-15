3e62b8e2219ff20254975d0317ec0a8098dbf3d9
nicholaskemery <68763075+nicholaskemery@users.noreply.github.com>
2020-08-15 17:05:47 -0400

    Update fulltextgcp.py

--- fulltextgcp.py
+++ fulltextgcp.py
@@ -1,10 +1,13 @@
 from arxiv_public_data import fulltext
+from google.cloud import storage
 
 import multiprocessing
 
-max_cpu = multiprocessing.cpu_count()
+max_cpu = 2
+
+storage_client = storage.Client()
 
 if __name__ == '__main__':
-    fulltext.convert_directory_parallel(path="gs://nkebucket-arxiv-ai/arxiv-pdf/", processes=max_cpu-2, timelimit=2*60)
+    fulltext.convert_directory_parallel(path=storage_client.bucket("gs://nkebucket-arxiv-ai/arxiv-pdf/"), processes=2, timelimit=2*60)
     #
-    fulltext.convert_directory(path="gs://nkebucket-arxiv-texts/", timelimit=2*60)
+    fulltext.convert_directory(path=storage_client.bucket("gs://nkebucket-arxiv-texts/"), timelimit=2*60)
