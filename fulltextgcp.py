5e38d26a3ffb75b6536567299e79d1e33b7e8bfb
nicholaskemery <68763075+nicholaskemery@users.noreply.github.com>
2020-08-15 17:27:08 -0400

    Update fulltextgcp.py

--- fulltextgcp.py
+++ fulltextgcp.py
@@ -8,6 +8,6 @@ max_cpu = 2
 storage_client = storage.Client()
 
 if __name__ == '__main__':
-    fulltext.convert_directory_parallel(path=storage_client.bucket("gs://nkebucket-arxiv-ai/arxiv-pdf/"), processes=2, timelimit=2*60)
+    fulltext.convert_directory_parallel(path=storage_client.bucket("gs://nkebucket-arxiv-ai/arxiv-pdf"), processes=2, timelimit=2*60)
     #
-    fulltext.convert_directory(path=storage_client.bucket("gs://nkebucket-arxiv-texts/"), timelimit=2*60)
+    fulltext.convert_directory(path=storage_client.bucket("gs://nkebucket-arxiv-texts"), timelimit=2*60)
